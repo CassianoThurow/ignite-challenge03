1c6d9e62fdde92f4b215a3d8ee0db36e53134b9e
Cassiano Thurow <cassiano.thurow@gmail.com>
2021-09-21 15:39:04 -0300

    🎉 styling home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +29,35 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Titulo</strong>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Cassiano Thurow
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
